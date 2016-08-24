b1189ecb4271ddf6bb2d6d42dc0f3190f4fcd03b
JAMES MOON <SENDJMOON@GMAIL.COM>
2016-08-24 00:09:50 -0700

    modularity to routes

--- lab-james/app/index.js
+++ lab-james/app/index.js
@@ -4,29 +4,10 @@ require('!!file?name=[name].[ext]!./html/index.html');
 require('./scss/base.scss');
 
 const angular = require('angular');
+const route_config = require('./routes/route_config');
 const imageApp = angular.module('imageApp', [require('angular-route')]);
+
+imageApp.config(route_config);
 
 require('./controllers')(imageApp);
 require('./components')(imageApp);
-
-imageApp.config(['$routeProvider', function($route) {
-  $route
-    .when('/', {
-      template: require('./html/home.html'),
-      controller: 'ImageController',
-      controllerAs: 'ic'
-    })
-    .when('/mugshot', {
-      template: require('./html/mugshot.html'),
-      controller: 'ImageController',
-      controllerAs: 'ic'
-    })
-    .when('/fullsize/:id', {
-      template: require('./html/fullsize.html'),
-      controller: 'ImageController',
-      controllerAs: 'ic'
-    })
-    .otherwise({
-      redirectTo: '/error'
-    });
-}]);
