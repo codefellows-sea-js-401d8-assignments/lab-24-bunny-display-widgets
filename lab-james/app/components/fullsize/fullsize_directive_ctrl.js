dca2d508c0aa0644f01ee8688b060807db15ec01
JAMES MOON <SENDJMOON@GMAIL.COM>
2016-08-23 23:55:26 -0700

    added console.log message in fullsize_directive_ctrl.js

--- lab-james/app/components/fullsize/fullsize_directive_ctrl.js
+++ lab-james/app/components/fullsize/fullsize_directive_ctrl.js
@@ -2,6 +2,7 @@
 
 module.exports = function(app) {
   app.controller('FullsizeController',['$routeParams', function($routeParams) {
+    console.log('Activated the FullsizeController');
     if ($routeParams.id === this.id) this.showPicture = true;
   }]);
 };
