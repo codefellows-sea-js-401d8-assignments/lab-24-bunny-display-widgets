f036470e15837569a772ba79a2718d17ba40480e
dylanjsa90 <dylanjsanders1@gmail.com>
2016-08-25 13:37:48 -0700

    removed extra directive ctrls

--- lab-dylan/app/components/fullsize_component/fullsize_directive_ctrl.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-module.exports = function(app) {
-  app.controller('FullsizeComponentController', function() {
-    this.description = this.description || 'no description';
-    this.url = this.url || 'no link';
-  });
-};
--- lab-dylan/app/components/text_component/text_directive_ctrl.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-module.exports = function(app) {
-  app.controller('TextComponentController', function() {
-    this.description = this.description || 'no description';
-    this.url = this.url || 'no link';
-  });
-};
