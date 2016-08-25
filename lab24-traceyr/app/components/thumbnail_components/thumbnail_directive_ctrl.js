f5d4783e25d5ec2c5fcc821446be127a9e9edfba
Tracey <radcliffe.tracey@gmail.com>
2016-08-25 10:14:53 -0700

    thumnail working on home

--- lab24-traceyr/app/components/thumbnail_components/thumbnail_directive_ctrl.js
+++ lab24-traceyr/app/components/thumbnail_components/thumbnail_directive_ctrl.js
@@ -2,6 +2,8 @@
 
 module.exports = function(app) {
   app.controller('ThumbnailDirectiveController', function(){
-    this.contents = this.contents || 'No Content Found';
+    // this.contents = this.contents || 'No Content Found';
+    console.log('Am I loaded?');
+    console.log(this.image);
   });
 };
