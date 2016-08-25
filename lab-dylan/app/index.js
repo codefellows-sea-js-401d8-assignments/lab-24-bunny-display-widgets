55ec1dcdf6ab39a484ec9803029c7e1256a4cee1
dylanjsa90 <dylanjsanders1@gmail.com>
2016-08-25 13:34:24 -0700

    fixed styling

--- lab-dylan/app/index.js
+++ lab-dylan/app/index.js
@@ -1,4 +1,5 @@
 require('!!file?name=[name].[ext]!./html/index.html');
+require('./scss/base.scss');
 
 const angular = require('angular');
 const demoApp = angular.module('demoApp', [require('angular-route')]);
